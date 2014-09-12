2625693213128c8aa4792771466e05d821c08be5
svo <svo@qual.is>
2014-09-13 08:02:25 +1000

    Adding uberjar profile

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject qr "0.0.1-SNAPSHOT"
   :description "ShortURL - URL shortening service"
   :url "http://qr.qual.is/"
+
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.6.0"]
     [io.pedestal/pedestal.service "0.3.0"]
 
@@ -26,8 +28,14 @@
     [lein-cloverage "1.0.2"]]
 
   :min-lein-version "2.0.0"
-  :resource-paths ["config", "resources"]
-  :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "qr.server/run-dev"]}
-    :dependencies [[io.pedestal/pedestal.service-tools "0.3.0"]]}}
-  :main ^{:skip-aot true} qr.server)
 
+  :resource-paths ["config", "resources"]
+
+  :profiles {
+    :dev {:aliases {"run-dev" ["trampoline" "run" "-m" "qr.server/run-dev"]}
+      :dependencies [[io.pedestal/pedestal.service-tools "0.3.0"]]}
+    :uberjar {:main qr.server
+      :aot :all
+      :dependencies [[io.pedestal/pedestal.jetty "0.3.0"]]}}
+
+  :main ^{:skip-aot true} qr.server)
